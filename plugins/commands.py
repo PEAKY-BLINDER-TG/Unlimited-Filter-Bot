59d7c90ac77626b3a02d47c10943b6ea90dd7ba0
Peaky blinder <85233139+wasimfaris2@users.noreply.github.com>
2021-08-03 22:12:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,12 +248,11 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BACK", callback_data="start_data"),
+                    InlineKeyboardButton("CLOSE", callback_data="close_data"),
                 ]
             ]
         ),
