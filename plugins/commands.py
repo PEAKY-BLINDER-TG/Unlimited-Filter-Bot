c4d59d0fb7658efe55e2d648369eb037e3fde5d9
Peaky blinder <85233139+wasimfaris2@users.noreply.github.com>
2021-08-04 12:23:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -211,8 +211,7 @@ async def bot_status(client,message):
 
 @trojanz.on_message(filters.command('start') & filters.private)
 async def start(client, message):
-    await message.send_photo(
-        photo="https://telegra.ph/file/8a0defaa723e0605b6099.jpg",
+    await message.reply_text(
         text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
